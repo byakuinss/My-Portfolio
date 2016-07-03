236e1beb3c60000d513354f154dc0dd6355bcacd
byakuinss <grace.chinhanyu@gmail.com>
2016-07-03 23:06:15 +0800

    16.07.03

---
+++ js/about.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$('#accordion').accordion({
+		heightStyle: "content",
+		collapsible: true
+	});
+	$('#more').hide();
+	$('.read-more').click(function(){
+		$('#more').toggle();
+	});
+	$('.prog').click(function(){
+		$('.java').animate({width: '100px'});
+		$('.ASPNET').animate({width: '40px'});
+		$('.HTML_CSS').animate({width: '70px'});
+		$('.VBA').animate({width: '80px'});
+		$('.SQL').animate({width: '120px'});
+		$('.Perl').animate({width: '60px'});
+	});
+	$('.prof').click(function(){
+		$('.web_dev').animate({width: '70px'});
+		$('.test').animate({width: '70px'});
+		$('.admin').animate({width: '140px'});
+		$('.trouble').animate({width: '120px'});
+	});
+});
